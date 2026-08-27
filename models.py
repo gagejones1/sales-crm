373fe29fe7d66eb23c2f7b162c7e2713ff961894
Gage Jones <gagejones253@gmail.com>
2026-08-27 09:52:12 -0500

    Add customer database model

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+class Customer(Base):
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    active = Column(Boolean, default=True)
